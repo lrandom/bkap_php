f9b65c0358fa7e19144d72c1f4e915e413856341
lrandom <luann4099@gmail.com>
2022-08-01 21:25:22 +0700

    validate demo

--- bai5/login.php
+++ bai5/login.php
@@ -1,24 +1,61 @@
-<?php 
-if(isset($_POST['email'])){
-  echo "Dữ liệu bạn đã gửi lên là\n";
-  echo $_POST['email'];
-  echo "\n";
-  echo $_POST['password'];
+<?php
+$errors = [];
+function validate(){
+    if (empty($_POST['email'])) {
+        $errors['email'] = 'Vui lòng điền email';
+        return false;
+    }
+
+    if (empty($_POST['password'])) {
+        $errors['password'] = 'Vui lòng điền mật khẩu';
+        return false;
+    }
+    return true;
+}
+if (isset($_POST['email'])) {
+    if(validate()){
+        //xử lý tiếp
+    }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <form method="POST" action="do-login.php">
-        <input type="email" name="email" placeholder="Vui lòng nhập email"/>
-        <input type="password" name="password" placeholder="Vui lòng nhập mật khẩu"/>
+    <form method="POST">
+        <div>
+            <input type="email" name="email" placeholder="Vui lòng nhập email" />
+            <?php if (isset($errors['email'])) {
+            ?>
+                <div class="error"><?php echo $errors['email']; ?></div>
+            <?php
+            } ?>
+        </div>
+
+        <div>
+            <input type="password" name="password" placeholder="Vui lòng nhập mật khẩu" />
+            <?php if (isset($errors['password'])) {
+            ?>
+                <div class="error"><?php echo $errors['password']; ?></div>
+            <?php
+            } ?>
+        </div>
+
         <button>Đăng nhập</button>
     </form>
+
+    <style>
+        .error {
+            color: red;
+        }
+    </style>
 </body>
+
 </html>
